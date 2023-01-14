1b99b3c1d7dbaff33165944af22a603e0ab99727
armory8854 <garrettsmith92@pm.me>
2023-01-14 16:53:15 -0500

    Added sqlite dep

--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [ 
       pkgs.python310
-      pkgs.python310Packages.feedparser 
+      pkgs.python310Packages.feedparser
+      pkgs.python310Packages.configparser
+      pkgs.sqlite
     ];
 }
 
