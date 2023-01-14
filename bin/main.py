20c383dc7e18fe75b70b551e65cb6df015247889
armory8854 <garrettsmith92@pm.me>
2023-01-14 16:52:58 -0500

    Working to pull rss entries into a db

--- bin/main.py
+++ bin/main.py
@@ -1,17 +1,30 @@
+# General Imports
 import os
-import json
-import feedparser
-import sqlite3
+import configparser
 
+# My imports
+from database_functions import db_add
+from rss import rss_retrieve
+
+# File variables go here
+config_file = "example.ini"
+database_file = "test/test.db"
+
+# Function like / function variables go here
+#config = configparser.ConfigParser()
+#config.read('./example.ini')
+
+# Test vars here
 rss_feed = str("https://nitter.net/ChimpSanctuary/rss")
-parsed_rss_feed = feedparser.parse(rss_feed)
 
-print(parsed_rss_feed)
-print(json.dumps(parsed_rss_feed["entries"], indent=2))
-#print(len(parsed_rss_feed))
+# Max articles to retrive at a time
+rss_counter = 0
+rss_counter_ceiling = 5
 
-def db_add():
-    with sqlite3.connect('./test/test.db') as conn:
-        conn = sqlite3.connect('./test/test.db')
-        cur = conn.cursor()
-        
+# Make the variables exist we plan on reassigning
+
+while rss_counter < rss_counter_ceiling:
+    rss_function = rss_retrieve(rss_feed, rss_counter)
+    author, post, published_date, id_url = rss_function
+    db_add(database_file, author, post, published_date, id_url)
+    rss_counter = rss_counter + 1
